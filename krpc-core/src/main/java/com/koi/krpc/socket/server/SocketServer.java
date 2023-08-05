c59d8924703748f6634b3ea9976e7f5fa7c8564a
koi2000 <koi20000@163.com>
2023-08-05 21:17:00 +0800

    feat: 使用ThreadPoolFactory创建Socket版本的线程池

--- krpc-core/src/main/java/com/koi/krpc/socket/server/SocketServer.java
+++ krpc-core/src/main/java/com/koi/krpc/socket/server/SocketServer.java
@@ -6,6 +6,7 @@ import com.koi.krpc.enumeration.RpcError;
 import com.koi.krpc.exception.RpcException;
 import com.koi.krpc.registry.ServiceRegistry;
 import com.koi.krpc.serializer.CommonSerializer;
+import com.koi.krpc.util.ThreadPoolFactory;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -17,11 +18,6 @@ import java.util.concurrent.*;
 // Socket方式远程方法调用的提供者（服务端）
 public class SocketServer implements RpcServer {
     private static final Logger logger = LoggerFactory.getLogger(SocketServer.class);
-
-    private static final int CORE_POOL_SIZE = 5;
-    private static final int MAXIMUM_POOL_SIZE = 50;
-    private static final int KEEP_ALIVE_TIME = 60;
-    private static final int BLOCKING_QUEUE_CAPACITY = 100;
     private final ExecutorService threadPool;
     private CommonSerializer serializer;
     private RequestHandler requestHandler = new RequestHandler();
@@ -29,9 +25,7 @@ public class SocketServer implements RpcServer {
 
     public SocketServer(ServiceRegistry serviceRegistry) {
         this.serviceRegistry = serviceRegistry;
-        BlockingQueue<Runnable> workingQueue = new ArrayBlockingQueue<>(BLOCKING_QUEUE_CAPACITY);
-        ThreadFactory threadFactory = Executors.defaultThreadFactory();
-        threadPool = new ThreadPoolExecutor(CORE_POOL_SIZE, MAXIMUM_POOL_SIZE, KEEP_ALIVE_TIME, TimeUnit.SECONDS, workingQueue, threadFactory);
+        threadPool = ThreadPoolFactory.createDefaultThreadPool("socket-rpc-server");
     }
 
     @Override
